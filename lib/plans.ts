7de45c853c7c006cb08bff3007304624f82938a3
Your Name <your_email@example.com>
2025-11-15 12:34:39 +0100

    5 free plan uses (down from 8)

--- lib/plans.ts
+++ lib/plans.ts
@@ -17,7 +17,7 @@ export const PAID_PLANS = {
   },
 };
 
-export const FREE_PLAN_GENERATIONS = 8;
+export const FREE_PLAN_GENERATIONS = 5;
 export const NON_AUTH_FREE_LIMIT = 3;
 
 export type Plan = keyof typeof PAID_PLANS;
